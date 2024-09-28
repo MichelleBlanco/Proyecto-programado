665087e63d6c1db826118f256f1e8dbd4ac2c498
MichelleBlanco <162941760+MichelleBlanco@users.noreply.github.com>
2024-09-27 18:28:05 -0600

    Add files via upload

---
+++ jugar.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+def crear_cuarta_ventana():
+    # Crear una nueva ventana secundaria
+    cuarta_ventana = tk.Toplevel()
+    cuarta_ventana.title("Crear crucigrama")
+    cuarta_ventana.geometry("1558x900")
