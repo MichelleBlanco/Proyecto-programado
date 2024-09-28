05a8fe1e84de0ad3ff51bbeb27d98e5c74300a27
MichelleBlanco <162941760+MichelleBlanco@users.noreply.github.com>
2024-09-27 18:26:06 -0600

    Update ingresar.py
    actualizaciones

--- ingresar.py
+++ ingresar.py
@@ -1,62 +1,85 @@
 import tkinter as tk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
 def ingresar_palabra():
-    lista_palabras = []  # Lista para almacenar las palabras y definiciones
-    lista_usar = [] 
-    letras_divididas = []
-    numero = 1  
-
     # Crear una nueva ventana secundaria
     tercera_ventana = tk.Toplevel()
-    tercera_ventana.title("Añadir palabras")
-    tercera_ventana.geometry("1558x900") 
+    tercera_ventana.title("Crear crucigrama")
+    tercera_ventana.geometry("1558x900")
 
-    while True:
-        label = tk.Label(tercera_ventana, text= f"Ingrese la palabra #{numero}: ", font=("Courier", 30),bg="lightblue", fg="black")
-        label.pack(pady=20)
+    # Obtener el tamaño de la pantalla
+    screen_width = tercera_ventana.winfo_screenwidth()
+    screen_height = tercera_ventana.winfo_screenheight()
+    # Ajustar el tamaño de la ventana al tamaño de la pantalla
+    tercera_ventana.geometry(f"{screen_width}x{screen_height}")
 
+    #FONDO
+    image = Image.open("R.jpg")
+    image = image.resize((screen_width, screen_height), Image.Resampling.LANCZOS)
+    bg_image = ImageTk.PhotoImage(image)
 
-        entrada_nombre = tk.Entry(tercera_ventana, fg='grey')
-        entrada_nombre.pack(pady=25)
-       
-        #BOTONES
-        create_button = tk.Button(tercera_ventana, text="Crear Crucigrama", width=18, command=salir, font=("Courier", 14), bg="lightblue")
-        create_button.pack(pady=30)
+    # Crear el Label para el fondo
+    bg_label = tk.Label(tercera_ventana, image=bg_image)
+    bg_label.place(relwidth=1, relheight=1)  
 
-        if label != "":
-            while True:
-                definicion = input(f"Escriba la definición de '{label}': ")
-                if definicion == "": 
-                    print("Debe ingresar una definición")
-                else:
-                    lista_palabras.append([numero, label, definicion])
-                    lista_usar.append([label])
-                     
-                    break  # Salimos del bucle solo si hay una definición válida
-            numero += 1
-        
-        else:
-            label = tk.Label(tercera_ventana, text= f"Debe ingresar una palabra", font=("Courier", 30),bg="lightblue", fg="black")
-            label.pack(pady=20)
-            messagebox.showinfo("No se ha ingresado ninguna palabra.")
-            
+    label = tk.Label(tercera_ventana, text= "Defina el tamaño de la cuadricula, en el eje x, y, z: ", font=("Courier", 30),bg="lightblue", fg="black")
+    label.pack(pady=20)
 
-        for sublista in lista_usar:
-            palabra = sublista[0]  # Tomar la primera (y única) palabra de la sublista
-            letras = list(palabra)  # Convertir la palabra en una lista de letras
-            letras_divididas.append(letras)
+    eje_x = tk.Entry(tercera_ventana, fg='grey')
+    eje_x.place(x=500, y=100)
+    eje_y = tk.Entry(tercera_ventana, fg='grey')
+    eje_y.place(x=700, y=100)
+    eje_z = tk.Entry(tercera_ventana, fg='grey')
+    eje_z.place(x=900, y=100)
+
+    label = tk.Label(tercera_ventana, text= "Ingrese una palabra: ", font=("Courier", 30),bg="lightblue", fg="black")
+    label.pack(pady=75)
+
+    entrada_palabra = tk.Entry(tercera_ventana, fg='grey')
+    entrada_palabra.pack(pady=25)
     
-        if lista_palabras:
-            print("\nLista de palabras y definiciones:")
-            for item in lista_palabras:
-                print(f"{item[0]}. Palabra: {item[1]}, Definición: {item[2]}")
-                
-        else:
-            messagebox.showinfo("No se ingresaron palabras.")
+    label = tk.Label(tercera_ventana, text= "Ingrese la definición: ", font=("Courier", 30),bg="lightblue", fg="black")
+    label.pack(pady=20)
+    entrada_definicion = tk.Entry(tercera_ventana, fg='grey')
+    entrada_definicion.pack(pady=25)
+    
+
+    #BOTONES
+    jugar_button = tk.Button(tercera_ventana, text="Jugar", width=15, command=jugar, font=("Courier", 14), bg="white", fg="black")
+    jugar_button.pack(pady=32)  
+    
+        # Agregar un botón para cerrar la ventana secundaria
+    close_button = tk.Button(tercera_ventana, text="Cerrar", width=15, command=tercera_ventana.destroy, font=("Courier", 15), bg="white", fg="black")
+    close_button.pack(pady=27)
+
+    def jugar():
+        while True:
+            x = eje_x.get()  # Obtener el nombre ingresado
+            y = eje_y.get()
+            z = eje_z.get()
+            palabra = entrada_palabra.get()  # Obtener el nombre ingresado
+            definicion = entrada_definicion.get()
+            if x and y and z and palabra and definicion:  # Verificar que no esté vacío
+                #diccionario_general= {"Versión":str(version),"nombre archivo":str(nombre), "Temática":str(tematica)}
+                #print(diccionario_general)
+                try:
+                    x = int(x)  # Intentar convertir a entero
+                    y = int(y)
+                    z = int(z)
+                    from jugar import crear_cuarta_ventana
+                    crear_cuarta_ventana()
+                except ValueError:  # Capturar si no es un número entero
+                    messagebox.showinfo("Ingrese un número")
+                    ingresar_palabra()
+            else:
+                messagebox.showinfo("Rellene los espacios")
+                ingresar_palabra()
+            break 
+
 
 # Ejecutar la función
-        tercera_ventana.mainloop()
+    tercera_ventana.mainloop()
 
 def salir():
     pass
